cf057f98727ee62b40ba42b539af1c7f52e0dcd3
Josh Cunningham <josh@joshcanhelp.com>
2021-01-10 08:52:10 -0800

    Adjust file structure to allow for easier installation

--- wp-rest-api-auth0-loader.php
+++ wp-rest-api-auth0-loader.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * Loader for joshcanhelp/wp-rest-api-auth0 MU-Plugin.
- *
- * Copy this file to wp-content/mu-plugins/
+ * Plugin Name: WP REST API Auth0
+ * Plugin URL: https://github.com/joshcanhelp/wp-rest-api-auth0
+ * Description: Protect your WordPress REST API with Auth0
+ * Version: 1.2.0
+ * Author: Josh Cunningham <josh@joshcanhelp.com>
+ * Author URI: https://www.joshcanhelp.com
  *
  * @package WpRestApiAuth0
  */
@@ -11,9 +14,9 @@ declare(strict_types=1);
 
 if (
     ! class_exists( 'Auth0\\SDK\\Helpers\\Tokens\\SymmetricVerifier' )
-    && is_file( __DIR__ . '/wp-rest-api-auth0/vendor/autoload.php' )
+    && is_file( __DIR__ . '/vendor/autoload.php' )
 ) {
-    require_once __DIR__ . '/wp-rest-api-auth0/vendor/autoload.php';
+    require_once __DIR__ . '/vendor/autoload.php';
 }
 
-require_once __DIR__ . '/wp-rest-api-auth0/wp-rest-api-auth0.php';
+require_once __DIR__ . '/src/wp-rest-api-auth0.php';
