f7c405e141ca5719ad1bee0983edc348901967ec
Josh Cunningham <josh@joshcanhelp.com>
2022-11-11 09:08:10 -0800

    feat: add debug mode

--- src/wp-rest-api-auth0.php
+++ src/wp-rest-api-auth0.php
@@ -22,6 +22,8 @@ add_filter( 'determine_current_user', __NAMESPACE__ . '\\determine_current_user'
 function determine_current_user( $user ) {
 	global $wpdb;
 
+	$debug_mode = defined('\AUTH0_API_DEBUG') && \AUTH0_API_DEBUG;
+
 	// Only checked, not saved or output anywhere.
 	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
 	$request_uri = wp_unslash( $_SERVER['REQUEST_URI'] ?? '' );
@@ -33,12 +35,18 @@ function determine_current_user( $user ) {
 
 	// Check if this is a WP REST API request.
 	if ( 1 !== preg_match( '/^\/' . rest_get_url_prefix() . '(.*)/', $request_uri ) ) {
+		if ($debug_mode) {
+			error_log('WP REST API Auth0: Not a REST API request');
+		}
 		return $user;
 	}
 
 	// Check for a token in the Authorization header.
 	$auth_header_parts = explode( ' ', $auth_header_raw );
 	if ( 'Bearer' !== $auth_header_parts[0] || empty( $auth_header_parts[1] ) ) {
+		if ($debug_mode) {
+			error_log('WP REST API Auth0: No access token found in the request');
+		}
 		return $user;
 	}
 	$access_token = $auth_header_parts[1];
@@ -58,12 +66,22 @@ function determine_current_user( $user ) {
 	} catch ( \Exception $e ) {
 		// An exception here means the token was invalid for some reason.
 		// While debugging, it might be helpful to output the exception message.
+		if ($debug_mode) {
+			error_log('WP REST API Auth0: Access token could not be verified - ' . $e->getMessage());
+		}
 		return null;
 	}
 
 	// We don't have a user to associate this call to.
 	if ( ! $decoded_token['sub'] ) {
+		if ($debug_mode) {
+			error_log('WP REST API Auth0: No sub claim found');
+		}
 		return null;
+	}
+
+	if ($debug_mode) {
+		error_log('WP REST API Auth0: Looking for Auth0 user ID ' . $decoded_token['sub']);
 	}
 
 	// Look for a WordPress user with the incoming Auth0 ID.
@@ -78,15 +96,27 @@ function determine_current_user( $user ) {
 
 	// Could not find a user with the incoming Auth0 ID.
 	if ( ! $wp_user ) {
+		if ($debug_mode) {
+			error_log('WP REST API Auth0: Could not find a user matching this the sub claim');
+		}
 		return null;
 	}
 
 	// Pull the scopes out of the access token and adjust the user accordingly.
 	$access_token_scopes = explode( ' ', $decoded_token['scope'] );
+
+	if ($debug_mode) {
+		error_log('WP REST API Auth0: Scopes requested - ' . $decoded_token['scope']);
+	}
+
 	foreach ( $wp_user->allcaps as $cap => $value ) {
 		if ( ! in_array( $cap, $access_token_scopes, true ) ) {
 			$wp_user->allcaps[ $cap ] = 0;
 		}
+	}
+
+	if ($debug_mode) {
+		error_log('WP REST API Auth0: Setting user as WP UID ' . $wp_user->ID);
 	}
 
 	// Set the current user as this modified user.
