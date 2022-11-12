66aafb4ae717d2ff6298ef318723df5d3332e8ab
Josh Cunningham <josh@joshcanhelp.com>
2022-11-12 09:10:44 -0800

    refactor: order of operations for auth header check

--- src/wp-rest-api-auth0.php
+++ src/wp-rest-api-auth0.php
@@ -28,18 +28,17 @@ function determine_current_user( $user ) {
 	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
 	$request_uri = wp_unslash( $_SERVER['REQUEST_URI'] ?? '' );
 
-	// Validated below with TokenVerifier.
-	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
-	$auth_header_raw = $_SERVER['HTTP_AUTHORIZATION'] ?? $_SERVER['REDIRECT_HTTP_AUTHORIZATION'] ?? '';
-	$auth_header_raw = wp_unslash( $auth_header_raw );
-
+	
 	// Check if this is a WP REST API request.
 	if ( 1 !== preg_match( '/^\/' . rest_get_url_prefix() . '(.*)/', $request_uri ) ) {
 		return $user;
 	}
-
+	
 	// Check for a token in the Authorization header.
-	$auth_header_parts = explode( ' ', $auth_header_raw );
+	// Validated below with TokenVerifier.
+	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
+	$auth_header_raw = wp_unslash( $_SERVER['HTTP_AUTHORIZATION'] ?? $_SERVER['REDIRECT_HTTP_AUTHORIZATION']);
+	$auth_header_parts = explode( ' ', $auth_header_raw ?? '' );
 	if ( 'Bearer' !== $auth_header_parts[0] || empty( $auth_header_parts[1] ) ) {
 		if ($debug_mode) {
 			error_log('WP REST API Auth0: No access token found in the request');
@@ -48,10 +47,13 @@ function determine_current_user( $user ) {
 	}
 	$access_token = $auth_header_parts[1];
 
-	// From this point on, we're going to treat the request as OAuth2 protected.
-	// If we cannot validate the token for some reason, the request is processed without auth.
+	////
+	/// From this point on, we're going to treat the request as OAuth2 protected.
+	/// If we cannot validate the token for some reason, the request is processed without auth.
+	//
 
-	// Verify the incoming access token.
+	// Verify the incoming JWT access token.
+	// Auth0-generated access tokens for users will be ID token shaped.
 	$token_verifier = new \WP_Auth0_IdTokenVerifier(
 		'https://' . \AUTH0_DOMAIN . '/',
 		\AUTH0_API_AUDIENCE,
@@ -61,8 +63,7 @@ function determine_current_user( $user ) {
 	try {
 		$decoded_token = $token_verifier->verify( $access_token );
 	} catch ( \Exception $e ) {
-		// An exception here means the token was invalid for some reason.
-		// While debugging, it might be helpful to output the exception message.
+		// An exception here means the token was invalid for some reason and cannot be accepted.
 		if ($debug_mode) {
 			error_log('WP REST API Auth0: Access token could not be verified - ' . $e->getMessage());
 		}
