edf8b069e46131eb41531c8db773557f6ece7edb
Josh Cunningham <josh@joshcanhelp.com>
2023-10-10 17:43:02 -0700

    refactor: add phpcs rules and make fixes

--- src/wp-rest-api-auth0.php
+++ src/wp-rest-api-auth0.php
@@ -22,38 +22,37 @@ add_filter( 'determine_current_user', __NAMESPACE__ . '\\determine_current_user'
 function determine_current_user( $user ) {
 	global $wpdb;
 
-	$debug_mode = defined('\AUTH0_API_DEBUG') && \AUTH0_API_DEBUG;
+	$debug_mode = defined( '\AUTH0_API_DEBUG' ) && \AUTH0_API_DEBUG;
 
 	// Only checked, not saved or output anywhere.
 	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
 	$request_uri = wp_unslash( $_SERVER['REQUEST_URI'] ?? '' );
 
-	
 	// Check if this is a WP REST API request.
 	if ( 1 !== preg_match( '/^\/' . rest_get_url_prefix() . '(.*)/', $request_uri ) ) {
 		return $user;
 	}
-	
+
 	// Check for a token in the Authorization header.
 	// Validated below with TokenVerifier.
-	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
-	$auth_header = $_SERVER['Authorization'] ?? 
+	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+	$auth_header       = $_SERVER['Authorization'] ??
 		$_SERVER['authorization'] ??
-		$_SERVER['HTTP_AUTHORIZATION'] ?? 
+		$_SERVER['HTTP_AUTHORIZATION'] ??
 		$_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
-	$auth_header_raw = wp_unslash( $auth_header );
+	$auth_header_raw   = wp_unslash( $auth_header );
 	$auth_header_parts = explode( ' ', $auth_header_raw ?? '' );
-	if ( 'bearer' !== strtolower($auth_header_parts[0]) || empty( $auth_header_parts[1] ) ) {
-		if ($debug_mode) {
-			error_log('WP REST API Auth0: No access token found in the request');
+	if ( 'bearer' !== strtolower( $auth_header_parts[0] ) || empty( $auth_header_parts[1] ) ) {
+		if ( $debug_mode ) {
+			error_log( 'WP REST API Auth0: No access token found in the request' );
 		}
 		return $user;
 	}
 	$access_token = $auth_header_parts[1];
 
-	////
-	/// From this point on, we're going to treat the request as OAuth2 protected.
-	/// If we cannot validate the token for some reason, the request is processed without auth.
+	//
+	// From this point on, we're going to treat the request as OAuth2 protected.
+	// If we cannot validate the token for some reason, the request is processed without auth.
 	//
 
 	// Verify the incoming JWT access token.
@@ -68,28 +67,28 @@ function determine_current_user( $user ) {
 		$decoded_token = $token_verifier->verify( $access_token );
 	} catch ( \Exception $e ) {
 		// An exception here means the token was invalid for some reason and cannot be accepted.
-		if ($debug_mode) {
-			error_log('WP REST API Auth0: Access token could not be verified - ' . $e->getMessage());
+		if ( $debug_mode ) {
+			error_log( 'WP REST API Auth0: Access token could not be verified - ' . $e->getMessage() );
 		}
 		return null;
 	}
 
 	// We don't have a user to associate this call to.
 	if ( ! $decoded_token['sub'] ) {
-		if ($debug_mode) {
-			error_log('WP REST API Auth0: No sub claim found in the access token');
+		if ( $debug_mode ) {
+			error_log( 'WP REST API Auth0: No sub claim found in the access token' );
 		}
 		return null;
 	}
 
-	if ($debug_mode) {
-		error_log('WP REST API Auth0: Looking for Auth0 user ID ' . $decoded_token['sub'] . '...');
+	if ( $debug_mode ) {
+		error_log( 'WP REST API Auth0: Looking for Auth0 user ID ' . $decoded_token['sub'] . '...' );
 	}
 
 	// Look for a WordPress user with the incoming Auth0 ID.
-	if ('client-credentials' === $decoded_token['gty']) {
-		$m2m_client_id = str_replace('@clients', '', $decoded_token['sub']);
-		$wp_user = get_user_by('login', $m2m_client_id );
+	if ( 'client-credentials' === $decoded_token['gty'] ) {
+		$m2m_client_id = str_replace( '@clients', '', $decoded_token['sub'] );
+		$wp_user       = get_user_by( 'login', $m2m_client_id );
 	} else {
 		$wp_user = current(
 			get_users(
@@ -103,18 +102,18 @@ function determine_current_user( $user ) {
 
 	// Could not find a user with the incoming Auth0 ID.
 	if ( ! $wp_user ) {
-		if ($debug_mode) {
-			error_log('WP REST API Auth0: Could not find a user matching this the sub claim');
+		if ( $debug_mode ) {
+			error_log( 'WP REST API Auth0: Could not find a user matching this the sub claim' );
 		}
 		return null;
 	}
 
 	// Pull the scopes out of the access token and adjust the user accordingly.
-	if ($decoded_token['scope']) {
+	if ( $decoded_token['scope'] ) {
 		$access_token_scopes = explode( ' ', $decoded_token['scope'] );
 
-		if ($debug_mode) {
-			error_log('WP REST API Auth0: Scopes requested - ' . $decoded_token['scope']);
+		if ( $debug_mode ) {
+			error_log( 'WP REST API Auth0: Scopes requested - ' . $decoded_token['scope'] );
 		}
 
 		foreach ( $wp_user->allcaps as $cap => $value ) {
@@ -124,8 +123,8 @@ function determine_current_user( $user ) {
 		}
 	}
 
-	if ($debug_mode) {
-		error_log('WP REST API Auth0: Setting user as WP UID ' . $wp_user->ID);
+	if ( $debug_mode ) {
+		error_log( 'WP REST API Auth0: Setting user as WP UID ' . $wp_user->ID );
 	}
 
 	// Set the current user as this modified user.
